a864f48857db3331a41b6af54c98f8575938b2bf
Cendikia122 <97296042+Cendikia122@users.noreply.github.com>
2025-03-07 06:37:21 +0700

    Repair v1.2

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,4 @@
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: "export",
-    basePath: process.env.BASE_PATH ? process.env.BASE_PATH : "",
-    assetPrefix: process.env.URL ? process.env.URL : undefined,
-  };
-  
-  export default nextConfig;
-  
+const nextConfig = {};
+
+export default nextConfig;
